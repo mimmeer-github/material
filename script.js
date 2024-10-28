2abff843e86a3abd5920590c93da8883656a0436
mimmeer <175632698+mimmeer-github@users.noreply.github.com>
2024-10-28 11:29:27 +0000

    Update script.js

--- script.js
+++ script.js
@@ -12,12 +12,20 @@ buttons.forEach(button => {
         const math = new Function('return ' + expression);
         const resultValue = math();
         result.value = resultValue;
+
+        // Add the calculation to the history array
+        historyArray.push(`${expression} = ${resultValue}`);
+
+        // Update the history element in the HTML
+        updateHistory();
       } catch (error) {
         result.value = 'Error';
       }
     } else if (value === 'clear') {
       res = '';
       result.value = '';
+    } else if (value === 'bksp') {
+      result.value = result.value.slice(0, -1);
     } else {
       res += value;
       result.value = res;
